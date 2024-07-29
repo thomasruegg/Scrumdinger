5254949b71220762fbec1a7d3ef2ed0ac0b9b4f6
thomasruegg <hello@thomasruegg.com>
2024-07-29 11:54:49 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Thomas Rüegg on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
