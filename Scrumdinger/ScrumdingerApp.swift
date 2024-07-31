d6170895acedbf558b05f5da1e13dd82f33ae123
thomasruegg <hello@thomasruegg.com>
2024-07-31 16:36:14 +0200

    implement persistence

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -9,9 +9,26 @@ import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
+    @StateObject private var store = ScrumStore()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ScrumsView(scrums: $store.scrums) {
+                Task {
+                    do {
+                        try await store.save(scrums: store.scrums)
+                    } catch {
+                        fatalError(error.localizedDescription)
+                    }
+                }
+            }
+                .task {
+                    do {
+                        try await store.load()
+                    } catch {
+                        fatalError(error.localizedDescription)
+                    }
+                }
         }
     }
 }
